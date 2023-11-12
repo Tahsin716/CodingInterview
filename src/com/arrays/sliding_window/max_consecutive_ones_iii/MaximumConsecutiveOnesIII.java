2c8f0f0283206c9d06747258b465de837d5cd7c2
Tahsin Rashad <Tahsin716@users.noreply.github.com>
2023-11-12 19:54:01 +0600

    Update MaximumConsecutiveOnesIII.java

--- src/com/arrays/sliding_window/max_consecutive_ones_iii/MaximumConsecutiveOnesIII.java
+++ src/com/arrays/sliding_window/max_consecutive_ones_iii/MaximumConsecutiveOnesIII.java
@@ -1,22 +1,38 @@
 package com.arrays.sliding_window.max_consecutive_ones_iii;
 
 public class MaximumConsecutiveOnesIII {
+    // Function to find the length of the longest subarray with at most K zeros
     public int longestOnes(int[] A, int K) {
+        // Initialize variables: max length, start and end pointers, array length, and zero count
         int max = 0, start = 0, end = 0, length = A.length;
         int zeroCount = 0;
 
+        // Iterate through the array using the sliding window approach
         while (end < length) {
-            if (A[end] == 0) zeroCount++;
+            // Check if the current element is zero and update zero count
+            if (A[end] == 0) {
+                zeroCount++;
+            }
 
+            // If the number of zeros exceeds the allowed limit (K),
+            // move the start pointer to shrink the window
             if (zeroCount > K) {
-                if (A[start] == 0) zeroCount--;
+                // If the element at the start is zero, decrement zero count
+                if (A[start] == 0) {
+                    zeroCount--;
+                }
+                // Move the start pointer to the next position
                 start++;
             }
 
+            // Update the maximum length encountered so far
             max = Math.max(max, end - start + 1);
+
+            // Move the end pointer to expand the window
             end++;
         }
 
+        // Return the maximum length of the subarray with at most K zeros
         return max;
     }
 }
